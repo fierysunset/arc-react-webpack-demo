2c5be5d489fddfd0d6e36cd06244f6517dc08404
conchang <conchang@ebay.com>
2017-05-13 19:28:16 -0700

    add mobile-detect

--- src/app-server/index.js
+++ src/app-server/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import React from 'react';
 import ReactDOMServer from 'react-dom/server';
+import MobileDetect from 'mobile-detect';
 import HelloWorld from '../components/hello-world';
 import basePage from './base-page.js';
 
@@ -9,9 +10,26 @@ const initialState = {
 	title: 'ARC React/Webpack Demo'
 };
 
+const getDeviceInfo = (req) => {
+	const md = new MobileDetect(req.headers['user-agent']);
+
+	let deviceInfo = {
+		isMobile: md.phone() !== null,
+		isAndroid: md.os() === 'AndroidOS',
+		isIOS: md.os() === 'iOS',
+		isIPhone: md.is('iPhone')
+	};
+
+	console.log(md.os());
+
+	return deviceInfo;
+}
+
 router.get('/', function (req, res) {
+	const deviceInfo = getDeviceInfo(req);
+
 	const html = ReactDOMServer.renderToString(
-		<HelloWorld/>
+		<HelloWorld />
 	);
 	res.status(200).send(basePage(html, initialState));
 });
