d6c2bb6f497ebacb124576ef5c3a2c944be71d4c
conchang <conchang@ebay.com>
2017-05-31 20:28:39 -0700

    remove whitespace around markup to remove browser console warning from react

--- src/app-server/base-page.js
+++ src/app-server/base-page.js
@@ -11,9 +11,7 @@ const basePage = (html, initialState) => {
             <link rel="stylesheet" href="../DIST/style.css">
         </head>
         <body>
-            <div id="main-content">
-                ${html}
-            </div>
+            <div id="main-content">${html}</div>
 
             <script>
                 window.__INITIAL_STATE__ = ${JSON.stringify(initialState)}
