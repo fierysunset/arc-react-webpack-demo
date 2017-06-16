af0dc899db2b956c00c95880f648b611d94ce0aa
Michael Rawlings <ml.rawlings@gmail.com>
2017-06-16 00:20:30 -0700

    clean up code a bit

--- src/app-server/router.js
+++ src/app-server/router.js
@@ -3,35 +3,29 @@ import AppLayout from '../components/app-layout';
 import basePage from './base-page.js';
 import express from 'express';
 import { FlagProvider } from 'arc-react';
-import fs from 'fs';
 import MobileDetect from 'mobile-detect';
 import React from 'react';
 import ReactDOMServer from 'react-dom/server';
 
 const router = express.Router();
-let deviceInfo = {};
-let initialState = {
-	title: 'ARC React/Webpack Demo'
-};
 
 // Store any necessary device details into an object for later use
 const getDeviceInfo = (req) => {
 	const md = new MobileDetect(req.headers['user-agent']);
 
-	deviceInfo = {
+	return {
 		desktop: md.phone() === null,
 		mobile: md.phone() !== null,
 		android: md.os() === 'AndroidOS',
 		ios: md.os() === 'iOS',
 		iphone: md.is('iPhone')
 	};
-
-	return deviceInfo;
 }
 
 // Create array of flags to pass to arc resolver module to get output path
-const getFlags = () => {
+const getFlags = (req) => {
 	let flags = [];
+	let deviceInfo = getDeviceInfo(req);
 
 	for (let key in deviceInfo) {
 		if (deviceInfo[key]) {
@@ -43,26 +37,29 @@ const getFlags = () => {
 }
 
 // Get output path based on flags from the user's device info
-const getOutputPath = () => {
-	let outputPath = '';
-	const flags = getFlags();
-
+const getOutputPath = (flags) => {
 	// Use arc-resolver to find the directory in the same location that matches the most flags
-	outputPath = arcResolver.adaptResource('DIST/default', flags);
-
-	return outputPath;
+	return arcResolver.adaptResource('DIST/default', flags);
 }
 
 router.get('/', function (req, res) {
-	initialState.deviceInfo = getDeviceInfo(req);
-	initialState.outputPath = getOutputPath();
+	const flags = getFlags(req);
+	const outputPath = getOutputPath(flags);
+	const initialState = {};
 
-	const html = ReactDOMServer.renderToString(
-		<FlagProvider flags={getFlags()}>
+	const appHtml = ReactDOMServer.renderToString(
+		<FlagProvider flags={flags}>
 			<AppLayout initialState={initialState}/>
 		</FlagProvider>
 	);
-	res.status(200).send(basePage(html, initialState));
+
+	const pageHtml = basePage({
+		title: 'ARC React/Webpack Demo',
+		appHtml,
+		outputPath
+	});
+
+	res.status(200).send(pageHtml);
 });
 
 export default router;
