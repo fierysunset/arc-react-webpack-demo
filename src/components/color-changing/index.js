91058c10c6aef55d95e8af31148b15e78c9e7671
Connie C Chang <conniecchang@gmail.com>
2017-06-19 03:48:57 -0700

    remove unnecessary noop loader

--- src/components/color-changing/index.js
+++ src/components/color-changing/index.js
@@ -6,7 +6,7 @@ export default class ColorChanging extends Component {
     render() {
         return (
             <div className="color-changing">
-                The text here will be <span className="color-text"></span> in <span className="device-type"></span>
+                This text is <span className="color-text"></span> in <span className="device-type"></span>
             </div>
         );
     }
