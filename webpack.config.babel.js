f24ea722af89cd8f76fa5ab497d4b88b11987fc9
Connie C Chang <conniecchang@gmail.com>
2017-06-21 05:01:52 -0700

    remove extract html plugin

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,6 +1,5 @@
 import arcWebpack from 'arc-webpack';
 import ExtractTextWebpackPlugin from 'extract-text-webpack-plugin';
-import HtmlWebpackPlugin from 'html-webpack-plugin';
 import nodeExternals from 'webpack-node-externals';
 import path from 'path';
 import webpack from 'webpack';
@@ -43,8 +42,7 @@ const clientConfig = (flags) => ({
         }]
     },
     plugins: [
-        new ExtractTextWebpackPlugin('style.css'),
-        new HtmlWebpackPlugin()
+        new ExtractTextWebpackPlugin('style.css')
     ]
 });
 
